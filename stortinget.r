ab412bf8d2eeaccb157ed188f0fb4cce03199280
François <briatte@gmail.com>
2014-08-04 23:52:20 +0200

    viz

--- stortinget.r
+++ stortinget.r
@@ -1,9 +1,13 @@
 library(downloader)
+library(network)
 library(XML)
 library(stringr)
 library(tnet)
 library(GGally)
 library(rgexf)
+
+plot = FALSE
+gexf = FALSE
 
 colors = c(
   "Sosialistisk Venstreparti" = "#E41A1C", # Left party, red
@@ -192,6 +196,7 @@ for(ii in unique(names(t))) {
   
   n = network(edges[, 1:2 ], directed = FALSE)
   n %n% "title" = paste("Stortinget", paste0(range(unique(data$years)), collapse = " to "))
+  n %n% "n_bills" = nrow(data)
   
   cat(network.size(n), "nodes")
   
@@ -221,8 +226,8 @@ for(ii in unique(names(t))) {
   nn = graph.edgelist(as.matrix(edges[, 1:2 ]), directed = FALSE)
   E(nn)$weight = edges[, 3]
   
-  i = gsub("(.*) \\[ (.*) \\]", "\\2", V(nn)$name)
-  i[ i %in% c("ROSSEM", "LDD", "FN", "", "INDEP") ] = NA # very small groups
+  i = s[ V(nn)$name, "party" ]
+  i[ i %in% c("Independent") ] = NA # unaffiliateds
   
   nn = nn - which(is.na(i))
   i = as.numeric(factor(i[ !is.na(i) ]))
@@ -259,7 +264,7 @@ for(ii in unique(names(t))) {
   n %n% "distance" = mean(wdeg$distance, na.rm = TRUE)
   
   n %v% "clustering" = wdeg$clustering    # local
-  # n %n% "clustering" = clustering_w(tnet) # global
+  n %n% "clustering" = clustering_w(tnet) # global
   
   party = n %v% "party"
   names(party) = network.vertex.names(n)
@@ -272,90 +277,117 @@ for(ii in unique(names(t))) {
   
   print(table(n %v% "party", exclude = NULL))
   
-  q = unique(quantile(n %v% "degree"))
-  n %v% "size" = as.numeric(cut(n %v% "degree", q, include.lowest = TRUE))
-  g = suppressWarnings(ggnet(n, size = 0, segment.alpha = 1/2, # mode = "kamadakawai",
-                             segment.color = party) +
-                         geom_point(alpha = 1/3, aes(size = n %v% "size", color = n %v% "party")) +
-                         geom_point(alpha = 1/2, aes(size = min(n %v% "size"), color = n %v% "party")) +
-                         scale_size_continuous(range = c(6, 12)) +
-                         scale_color_manual("", values = colors, breaks = order) +
-                         theme(legend.key.size = unit(1, "cm"),
-                               legend.text = element_text(size = 16)) +
-                         guides(size = FALSE, color = guide_legend(override.aes = list(alpha = 1/3, size = 6))))
-  
-  ggsave(paste0("net_", gsub("\\s", "_", ii), "_", nrow(data), ".pdf"), g, width = 12, height = 9)
-  # assign(paste0("net", substr(ii, 1, 4)), n)
+  if(plot) {
+    
+    q = unique(quantile(n %v% "degree"))
+    n %v% "size" = as.numeric(cut(n %v% "degree", q, include.lowest = TRUE))
+    g = suppressWarnings(ggnet(n, size = 0, segment.alpha = 1/2, # mode = "kamadakawai",
+                               segment.color = party) +
+                           geom_point(alpha = 1/3, aes(size = n %v% "size", color = n %v% "party")) +
+                           geom_point(alpha = 1/2, aes(size = min(n %v% "size"), color = n %v% "party")) +
+                           scale_size_continuous(range = c(6, 12)) +
+                           scale_color_manual("", values = colors, breaks = order) +
+                           theme(legend.key.size = unit(1, "cm"),
+                                 legend.text = element_text(size = 16)) +
+                           guides(size = FALSE, color = guide_legend(override.aes = list(alpha = 1/3, size = 6))))
+    
+    ggsave(paste0("net_", gsub("\\s", "_", ii), "_", nrow(data), ".pdf"), g, width = 12, height = 9)
+    
+  }
 
+  assign(paste0("net_", ii), n)
+  
   # gexf
-  
-  rgb = t(col2rgb(colors[ names(colors) %in% as.character(n %v% "party") ]))
-  mode = "fruchtermanreingold"
-  meta = list(creator = "rgexf",
-              description = paste0(mode, " placement"),
-              keywords = "Parliament, Sweden")
-  
-  people = data.frame(url = n %v% "uid",
-                      name = network.vertex.names(n), 
-                      party = n %v% "party",
-                      county = n %v% "county",
-                      born = n %v% "born",
-                      degree = n %v% "degree",
-                      distance = n %v% "distance",
-                      photo = n %v% "photo",
-                      stringsAsFactors = FALSE)
-  
-  node.att = c("url", "party", "county", "born", "degree", "distance", "photo")
-  node.att = cbind(label = people$name, people[, node.att ])
-  
-  people = data.frame(id = as.numeric(factor(people$name)),
-                      label = people$name,
-                      stringsAsFactors = FALSE)
-  
-  relations = data.frame(
-    source = as.numeric(factor(n %e% "source", levels = levels(factor(people$label)))),
-    target = as.numeric(factor(n %e% "target", levels = levels(factor(people$label)))),
-    weight = n %e% "weight"
-  )
-  relations = na.omit(relations)
-  
-  nodecolors = lapply(node.att$party, function(x)
-    data.frame(r = rgb[x, 1], g = rgb[x, 2], b = rgb[x, 3], a = .3 ))
-  nodecolors = as.matrix(rbind.fill(nodecolors))
-  
-  net = as.matrix.network.adjacency(n)
-  
-  # placement method (Kamada-Kawai best at separating at reasonable distances)
-  position = paste0("gplot.layout.", mode)
-  if(!exists(position)) stop("Unsupported placement method '", position, "'")
-  
-  position = do.call(position, list(net, NULL))
-  position = as.matrix(cbind(position, 1))
-  colnames(position) = c("x", "y", "z")
-  
-  # compress floats
-  position[, "x"] = round(position[, "x"], 2)
-  position[, "y"] = round(position[, "y"], 2)
-  
-  write.gexf(nodes = people,
-             edges = relations[, -3],
-             edgesWeight = relations[, 3],
-             nodesAtt = data.frame(label = as.character(node.att$label),
-                                   url = node.att$url,
-                                   party = node.att$party,
-                                   county = node.att$county,
-                                   born = node.att$born,
-                                   degree = node.att$degree,
-                                   distance = node.att$distance,
-                                   photo = node.att$photo,
-                                   stringsAsFactors = FALSE),
-             nodesVizAtt = list(position = position,
-                                color = nodecolors,
-                                size = round(node.att$degree)),
-             # edgesVizAtt = list(size = relations[, 3]),
-             defaultedgetype = "undirected", meta = meta,
-             output = paste0("net_", gsub("\\s", "_", ii), ".gexf"))
+  if(gexf) {
+    
+    rgb = t(col2rgb(colors[ names(colors) %in% as.character(n %v% "party") ]))
+    mode = "fruchtermanreingold"
+    meta = list(creator = "rgexf",
+                description = paste0(mode, " placement"),
+                keywords = "Parliament, Sweden")
+    
+    people = data.frame(url = n %v% "uid",
+                        name = network.vertex.names(n), 
+                        party = n %v% "party",
+                        county = n %v% "county",
+                        born = n %v% "born",
+                        degree = n %v% "degree",
+                        distance = n %v% "distance",
+                        photo = n %v% "photo",
+                        stringsAsFactors = FALSE)
+    
+    node.att = c("url", "party", "county", "born", "degree", "distance", "photo")
+    node.att = cbind(label = people$name, people[, node.att ])
+    
+    people = data.frame(id = as.numeric(factor(people$name)),
+                        label = people$name,
+                        stringsAsFactors = FALSE)
+    
+    relations = data.frame(
+      source = as.numeric(factor(n %e% "source", levels = levels(factor(people$label)))),
+      target = as.numeric(factor(n %e% "target", levels = levels(factor(people$label)))),
+      weight = n %e% "weight"
+    )
+    relations = na.omit(relations)
+    
+    nodecolors = lapply(node.att$party, function(x)
+      data.frame(r = rgb[x, 1], g = rgb[x, 2], b = rgb[x, 3], a = .3 ))
+    nodecolors = as.matrix(rbind.fill(nodecolors))
+    
+    net = as.matrix.network.adjacency(n)
+    
+    # placement method (Kamada-Kawai best at separating at reasonable distances)
+    position = paste0("gplot.layout.", mode)
+    if(!exists(position)) stop("Unsupported placement method '", position, "'")
+    
+    position = do.call(position, list(net, NULL))
+    position = as.matrix(cbind(position, 1))
+    colnames(position) = c("x", "y", "z")
+    
+    # compress floats
+    position[, "x"] = round(position[, "x"], 2)
+    position[, "y"] = round(position[, "y"], 2)
+    
+    write.gexf(nodes = people,
+               edges = relations[, -3],
+               edgesWeight = relations[, 3],
+               nodesAtt = data.frame(label = as.character(node.att$label),
+                                     url = node.att$url,
+                                     party = node.att$party,
+                                     county = node.att$county,
+                                     born = node.att$born,
+                                     degree = node.att$degree,
+                                     distance = node.att$distance,
+                                     photo = node.att$photo,
+                                     stringsAsFactors = FALSE),
+               nodesVizAtt = list(position = position,
+                                  color = nodecolors,
+                                  size = round(node.att$degree)),
+               # edgesVizAtt = list(size = relations[, 3]),
+               defaultedgetype = "undirected", meta = meta,
+               output = paste0("net_", gsub("\\s", "_", ii), ".gexf"))
+    
+  }
   
 }
+
+save(list = ls(pattern = "net_"), file = "stortinget.rda")
+
+m = data.frame(id = ls(pattern = "net_"),
+               d = sapply(ls(pattern = "net_"), function(x) network.density(get(x))),
+               n = sapply(ls(pattern = "net_"), function(x) get.network.attribute(get(x), "n_bills")),
+               m = sapply(ls(pattern = "net_"), function(x) get.network.attribute(get(x), "modularity")),
+               w = sapply(ls(pattern = "net_"), function(x) get.network.attribute(get(x), "modularity_walktrap")),
+               l = sapply(ls(pattern = "net_"), function(x) get.network.attribute(get(x), "modularity_louvain"))
+)
+m$r = m$m / apply(m[, c("w", "l") ], 1, max)
+
+g = qplot(data = m, y = n, label = gsub("net_", "", id), x = r, size = d, geom = "text") +
+  labs(y = "Number of bills\n", x = "\nEmpirical / Maximized Modularity") +
+  guides(size = FALSE) +
+  scale_size_continuous(range = c(4, 6)) +
+  theme_linedraw(16)
+
+ggsave("modularity.png", g, width = 9, height = 9)
 
 # have a nice day
