b7b2cf50cc2b4c0d0bd1ce6b9ced165ffe335151
François <briatte@gmail.com>
2015-08-12 14:56:54 +0200

    folders

--- make.r
+++ make.r
@@ -17,7 +17,9 @@ dir.create("photos", showWarnings = FALSE)
 if (file.exists("raw.zip"))
   unzip("raw.zip")
 
-dir.create("raw"    , showWarnings = FALSE)
+dir.create("raw"       , showWarnings = FALSE)
+dir.create("raw/mps"   , showWarnings = FALSE)
+dir.create("raw/bills" , showWarnings = FALSE)
 
 # parameters
 
